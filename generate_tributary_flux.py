b337e77282f92ddbf92d45e49f2894fdae1bd05f
Isabel Suhr <isabels@lclark.edu>
2015-10-14 23:56:12 -0700

    started thinking about how to calculate tributary flux

---
+++ generate_tributary_flux.py
@@ -0,0 +1,9 @@
+
+
+#NEED TO FIND TO CALCULATE THIS
+
+# - GPS coordinates of canonical flowline - ask Kiya about
+# - GPS coordinates of where tributaries flow into flowline
+# - velocities at where tributaries flow into flowline
+# - some estimate of glacier depth where tributaries flow in (from Nolan et al?)
+# - 
