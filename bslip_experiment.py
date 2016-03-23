93d741b351d9d565d27c445e1445cc232a0f57e5
Isabel Suhr <isabels@lclark.edu>
2016-03-23 12:06:11 -0700

    ran experiment, updated with new values to try

--- bslip_experiment.py
+++ bslip_experiment.py
@@ -4,8 +4,8 @@ import basic_model
 b0 = tools.load_nolan_bedrock()
 obs_surf = tools.load_first_guess_surface()
 
-start = [.001, .00125, .0015, .00175, .002]
-stop = [.0001, .00015, .0002, .00022, .00025, .0003]
+start = [.001, .001125, .00125, .001375, .0015]#[.001, .00125, .0015, .00175, .002]
+stop = [.000025, .00005, .000075, .0001]#[.0001, .00015, .0002, .00022, .00025, .0003]
 
 for i in start:
     for j in stop:
@@ -15,11 +15,11 @@ for i in start:
         for k in range(58):
             bslip.append(temp)
             temp += step
-        run1 = basic_model.isothermalISM(58, 1000, bslip, b0)
+        run1 = basic_model.isothermalISM(58, 1000, bslip, b0[:])
         for k in range(1500): #5000 years
             run1.timestep(1)
-            if(k%100==0): 
+            #if(k%100==0): 
                 #print 'on timestep', k
         print 'for start', i, 'and stop', j,'surface difference is', tools.calculate_surface_difference(run1.get_surface_elev() ,obs_surf)
 
-
+#first experiment gives lowest difference for start .00125 and stop .0001, updating start and stop again reflecting this
